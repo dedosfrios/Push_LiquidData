694972150f109c2821ca286db9e365208457c35d
Jota Galvan <jotagalvan@gmail.com>
2026-08-03 16:08:11 -0500

    Create 0_config.js

---
+++ 0_config.js
@@ -0,0 +1,10 @@
+// Constantes globales de producción
+const ENTORNO = {
+  MASTERFILE_ID: "1rkXKqPXCKkA9fWXzvQ3464Yhay0vaSf1EclglPdWUcM",
+  AIRSHIP_ID:    "1L0V5JALZimz0_-EZE0buFDCgs6HD8bzovYs4vkuGyY4",
+  ADOBE_ID:      "1Lhr8ibHKeWq0O9G1Jy9iYwQLgMKSgIEuh94mgJp4LUY",
+  
+  // EL NUEVO CONTENEDOR EXCLUSIVO PARA BI (Reemplaza con el ID de tu nuevo archivo)
+  DESTINO_BI_ID: "1nYZ9C0gLXxQuzQ8HALCcCVPJa8FeqErog-zDq98QRyc",
+  UMBRAL_MASIVO: 1000000 // 1M de impactos
+};
